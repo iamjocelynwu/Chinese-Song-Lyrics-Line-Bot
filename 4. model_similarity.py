dd2b5395bc00f72f421a5d0f500b179cb2ae6274
iamjocelynwu <98579075+iamjocelynwu@users.noreply.github.com>
2023-06-09 11:01:58 +0800

    4. model_similarity.py

--- 4. model_similarity.py
+++ 4. model_similarity.py
@@ -11,7 +11,7 @@ import random
 vectorizer_files = ['lyrics_vectorizer.pkl', 'title_vectorizer.pkl', 'singer_vectorizer.pkl']
 vector_files = ['lyrics_vector.pkl', 'title_vector.pkl', 'singer_vector.pkl']
 
-# 檢查是否有存檔，若有則載入向量化器和向量資料
+# 檢查是否有存檔，若有則載入向量資料
 if all(os.path.isfile(file) for file in vectorizer_files + vector_files):
     with open(vectorizer_files[0], 'rb') as f:
         lyrics_vectorizer = pickle.load(f)
@@ -35,9 +35,7 @@ else:
     lyrics_vectorizer = TfidfVectorizer()
     title_vectorizer = TfidfVectorizer()
     singer_vectorizer = TfidfVectorizer()
-    
-    #fit_transform()方法是用來訓練TF-IDF向量化器並將文本資料轉換成TF-IDF向量。在訓練階段，我們需要提供整個文本資料集來學習詞彙和計算詞彙的IDF值，因此在這裡我們將整個'歌手'列（這是一個pandas Series）轉換成unicode字符串格式（.values.astype('U')）。這麼做是因為當pandas Series包含非字符串（如NaN或其他Python對象）時，將其轉換成unicode可以確保TF-IDF向量化器可以正確地處理。
-    
+            
     lyrics_vector = lyrics_vectorizer.fit_transform(data['歌詞'].values.astype('U'))
     title_vector = title_vectorizer.fit_transform(data['歌曲名'].values.astype('U'))
     singer_vector = singer_vectorizer.fit_transform(data['歌手'].values.astype('U'))
@@ -65,11 +63,6 @@ def recommend_songs(query):
     query_title = " ".join(jieba.cut(query))
     query_singer = " ".join(jieba.cut(query))
 
-#在進行推薦時，我們的查詢（query_singer）已經是一個字符串。在這種情況下，我們只需要用已經訓練過的向量化器來轉換查詢，並不需要再進行.values.astype('U')的操作。這是因為我們已經知道這是一個字符串，並且我們只轉換一個單獨的查詢，而不是整個文本資料集。
-#fit_transform()：這個函數首先調用fit()方法來學習模型的參數（例如，資料的均值、標準差、最大最小值等），然後再將這些參數應用到數據集上，進行轉換（例如標準化、正則化等）。所以#fit_transform()是學習參數（fit）以及基於所學參數進行轉換（transform）的一個整合函數。
-#transform()：這個函數使用在之前的fit()方法中學習到的參數，來對給定數據進行轉換。transform()方法只進行轉換。
-#這兩個方法的主要區別在於，fit_transform()既學習參數也進行轉換，而transform()僅僅進行轉換。
-#這在訓練數據與測試數據的處理上有一個重要的應用：在訓練數據上我們應該使用fit_transform()方法，因為我們需要學習數據的參數並將它們應用在數據上。而在測試數據上，我們只需要使用transform()方法，用訓練數據學習到的參數來進行轉換。
     query_lyrics_vector = lyrics_vectorizer.transform([query_lyrics])
     query_title_vector = title_vectorizer.transform([query_title])
     query_singer_vector = singer_vectorizer.transform([query_singer])
@@ -87,7 +80,7 @@ def recommend_songs(query):
         lines = lyrics.split()
         matching_indices = [i for i, line in enumerate(lines) if query in line]
         matching_lines = [lines[i] for i in matching_indices]
-        # Add the previous and the next line of each matching line
+     
         for i in matching_indices:
             if i > 0:
                 matching_lines.append(lines[i-1])
